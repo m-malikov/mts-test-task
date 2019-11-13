15c686b0f846204625a91cd05af3a578f4dd7e05
m-malikov <malikov.ma@phystech.edu>
2019-11-13 18:06:56 +0300

    keys and minor changes

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from pprint import pprint
 
 import scripts
 
@@ -11,3 +12,4 @@ with open(os.path.join("raw_data", "three_minutes_tweets.json.txt"), "r") as f:
 scripts.normalize(conn)
 with open(os.path.join("raw_data", "AFINN-111.txt")) as f:
     scripts.fill_sentiment(conn, f)
+pprint(scripts.get_exremes(conn))
--- scripts.py
+++ scripts.py
@@ -5,15 +5,14 @@ import re
 def create_database(conn):
     cur = conn.cursor()
     cur.execute("""
-        CREATE TABLE tweets (name TINYTEXT, 
-                            tweet_text TEXT, 
-                            country_code TINYTEXT, 
-                            display_url TINYTEXT,
-                            lang TINYTEXT,
-                            created_at TIMESTAMP,
-                            location TEXT,
-                            tweet_sentiment REAL
-                            );
+        CREATE TABLE tweets (name TINYTEXT NOT NULL, 
+                             tweet_text TEXT NOT NULL, 
+                             country_code TINYTEXT, 
+                             display_url TINYTEXT PRIMARY KEY,
+                             lang TINYTEXT,
+                             created_at TIMESTAMP NOT NULL,
+                             location TEXT,
+                             tweet_sentiment REAL);
     """)
     conn.commit()
 
@@ -41,7 +40,7 @@ def insert_rows(conn, tweets_file):
     tweets = map(json.loads, tweets_file)
     tweets = filter(lambda t: "created_at" in t, tweets)
     rows = map(extract_fields, tweets)
-    cur.executemany("INSERT INTO tweets VALUES (?, ?, ?, ?, ?, ?, ?, ?)", rows)
+    cur.executemany("INSERT OR IGNORE INTO tweets VALUES (?, ?, ?, ?, ?, ?, ?, ?)", rows)
 
     conn.commit()
 
@@ -50,17 +49,33 @@ def normalize(conn):
 
     # country_code is defined by location
     cur.execute("""
-    CREATE TABLE locations AS
-    SELECT location as name, MAX(country_code) as country_code
-    FROM tweets WHERE location IS NOT NULL GROUP BY location;
+        CREATE TABLE locations (
+            name TEXT PRIMARY KEY,
+            country_code TINYTEXT
+        )
+    """)
+    cur.execute("""
+        INSERT INTO locations
+        SELECT location as name, MAX(country_code) as country_code
+        FROM tweets WHERE location IS NOT NULL GROUP BY location;
     """)
 
     # display_url dublicates name in it
     cur.execute("""
-    CREATE TABLE tweets_normalized AS
-    SELECT REPLACE(display_url, RTRIM(display_url, REPLACE(display_url, '/', '')), '') as id,
-        name, tweet_text, country_code, lang, created_at, location, tweet_sentiment
-    FROM tweets;
+        CREATE TABLE tweets_normalized (id TINYTEXT PRIMARY KEY,
+                                        name TINYTEXT NOT NULL, 
+                                        tweet_text TEXT NOT NULL, 
+                                        location TINYTEXT, 
+                                        lang TINYTEXT,
+                                        created_at TIMESTAMP NOT NULL,
+                                        tweet_sentiment REAL,
+                                        FOREIGN KEY(location) REFERENCES locations(name)); 
+    """)
+    cur.execute("""
+        INSERT INTO tweets_normalized
+        SELECT REPLACE(display_url, RTRIM(display_url, REPLACE(display_url, '/', '')), '') as id,
+            name, tweet_text, location, lang, created_at, tweet_sentiment
+        FROM tweets;
     """)
 
 def fill_sentiment(conn, afinn_file):
@@ -79,9 +94,37 @@ def fill_sentiment(conn, afinn_file):
         return score
 
     cur = conn.cursor()
-    cur.execute("SELECT id, tweet_text FROM tweets_normalized WHERE lang = 'en'")
+    cur.execute('SELECT display_url, tweet_text FROM tweets WHERE lang = "en"')
     for row in cur:
-        conn.execute('UPDATE tweets_normalized SET tweet_sentiment = ? WHERE id = ?',
+        conn.execute('UPDATE tweets SET tweet_sentiment = ? WHERE display_url = ?',
                     (str(rate(prepare_text(row[1]))), row[0]))
         
     conn.commit()
+
+def get_exremes(conn):
+    # There and after using simple bayesian average. Not sure about the right approach since the dataset is rather small.
+    def run_query(q):
+        cur = conn.cursor()
+        return cur.execute(q).fetchall()
+    
+    person_query = """
+       SELECT display_url, name, tweet_text FROM tweets WHERE name = (
+	        SELECT name FROM tweets GROUP BY name 
+			HAVING SUM(tweet_sentiment) IS NOT NULL 
+			ORDER BY SUM(tweet_sentiment) / (COUNT(tweet_sentiment) + 1) {} LIMIT 1
+        )   
+    """
+    location_query = """
+        SELECT {0} FROM tweets GROUP BY {0} 
+        HAVING SUM(tweet_sentiment) IS NOT NULL 
+        ORDER BY SUM(tweet_sentiment) / (COUNT(tweet_sentiment) + 1) {1} LIMIT 1
+    """
+
+    return {
+        "worst_by_person": run_query(person_query.format("ASC")),
+        "best_by_person": run_query(person_query.format("DESC")),
+        "worst_by_location": run_query(location_query.format("location", "ASC"))[0][0],
+        "best_by_location": run_query(location_query.format("location", "DESC"))[0][0],
+        "worst_by_country": run_query(location_query.format("country_code", "ASC"))[0][0],
+        "best_by_country": run_query(location_query.format("country_code", "DESC"))[0][0],
+    }
